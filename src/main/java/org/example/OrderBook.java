e9da012a4d6465d563b8d776fd86a16642d80b65
Kuba Kosiński <kosinski.jakub01@gmail.com>
2026-02-22 14:36:55 +0100

    add simplified matching engine

--- src/main/java/org/example/OrderBook.java
+++ src/main/java/org/example/OrderBook.java
@@ -16,4 +16,40 @@ public class OrderBook {
             return price <= bids.firstKey();
         }
     }
+
+    public List<Trade> matchingEngine() {
+        List<Trade> trades = new ArrayList<>();
+            while (!asks.isEmpty() && !bids.isEmpty() && bids.firstKey() >= asks.firstKey()) {
+               ArrayDeque<Order> askQue = asks.firstEntry().getValue();
+               ArrayDeque<Order> bidQue = bids.firstEntry().getValue();
+
+               Order ask = askQue.peekFirst();
+               Order bid = bidQue.peekFirst();
+
+
+                int quantity = Math.min(ask.getRemainingQuantity(), bid.getRemainingQuantity());
+                ask.fill(quantity);
+                bid.fill(quantity);
+
+                if (ask.getRemainingQuantity() == 0) {
+                    askQue.pollFirst();
+                }
+                if (askQue.isEmpty()) {
+                    asks.pollFirstEntry();
+                }
+
+                if (bid.getRemainingQuantity() == 0) {
+                    bidQue.pollFirst();
+                }
+                if (bidQue.isEmpty()) {
+                    bids.pollFirstEntry();
+                }
+                TradeInfo askTradeInfo = new TradeInfo(ask.getOrderId(), ask.getRemainingQuantity(), ask.getPrice());
+                TradeInfo bidTradeInfo = new TradeInfo(bid.getOrderId(), bid.getRemainingQuantity(), bid.getPrice());
+                Trade trade = new Trade(bidTradeInfo, askTradeInfo);
+                trades.add(trade);
+            }
+        return trades;
+    }
+
 }
